de0e0ddc32bfd1dd180c88fbf45c4b5e6945ffc4
Михаил <mcups@yandex.ru>
2017-08-30 12:02:52 +0300

    Добавлена activity для расчета Kvs. (reverted from commit 57f9a497b16cb25e5b6c13508d4032efa184704d)

--- app/src/main/java/com/example/hvaccalcmobile/MainActivity.java
+++ app/src/main/java/com/example/hvaccalcmobile/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.hvaccalcmobile;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    // Метод обработки нажатия на кнопку
-    public void openKvsActivity(View view){
-        // Создание объекта Intent
-        Intent intent = new Intent(this, KvsActivity.class);
-        startActivity(intent);
     }
 }
