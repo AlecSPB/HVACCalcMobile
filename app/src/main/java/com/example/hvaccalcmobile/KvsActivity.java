b9f6dcf7dd287da54944804b3f95ae1456a0c8d4
Михаил <mcups@yandex.ru>
2017-08-30 12:03:47 +0300

    Добавлена activity для расчета Kvs.

---
+++ app/src/main/java/com/example/hvaccalcmobile/KvsActivity.java
@@ -0,0 +1,15 @@
+package com.example.hvaccalcmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KvsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kvs);
+
+        setTitle("Расчет Kvs");
+    }
+}
