8b78f64eafe97eaebf3b9145122ecc7ff48eeaed
Михаил <mcups@yandex.ru>
2017-08-31 12:53:56 +0300

    Реализован функционал расчета круглых воздуховодов.

--- app/src/main/java/com/example/hvaccalcmobile/AirDuctCActivity.java
+++ app/src/main/java/com/example/hvaccalcmobile/AirDuctCActivity.java
@@ -5,27 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AirDuctActivity extends AppCompatActivity {
+public class AirDuctCActivity extends AppCompatActivity {
 
-    TextView flowRateTextView;
-    TextView widthTextView;
-    TextView heightTextView;
+    TextView diameterTextView;
     TextView temperatureTextView;
     TextView roughnessTextView;
-    TextView diameterLabelTextView;
-    TextView diameterTextView;
+    TextView flowRateTextView;
     TextView velocityLabelTextView;
     TextView velocityTextView;
     TextView pressureDropLabelTextView;
     TextView pressureDropTextView;
 
-    EditText flowRateEditText;
-    EditText widthEditText;
-    EditText heightEditText;
+
+    EditText diameterEditText;
     EditText temperatureEditText;
     EditText roughnessEditText;
+    EditText flowRateEditText;
 
     Button calculateButton;
     Button resetButton;
@@ -33,16 +32,14 @@ public class AirDuctActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_air_duct);
+        setContentView(R.layout.activity_air_duct_c);
 
-        setTitle("Расчет воздуховода");
+        setTitle("Расчет круглого воздуховода");
 
         flowRateTextView = (TextView) findViewById(R.id.flowRateTextView);
-        widthTextView = (TextView) findViewById(R.id.widthTextView);
-        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        diameterTextView = (TextView) findViewById(R.id.diameterTextView);
         temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
         roughnessTextView = (TextView) findViewById(R.id.roughnessTextView);
-        diameterLabelTextView = (TextView) findViewById(R.id.diameterLabelTextView);
         diameterTextView = (TextView) findViewById(R.id.diameterTextView);
         velocityLabelTextView = (TextView) findViewById(R.id.velocityLabelTextView);
         velocityTextView = (TextView) findViewById(R.id.velocityTextView);
@@ -50,15 +47,12 @@ public class AirDuctActivity extends AppCompatActivity {
         pressureDropTextView = (TextView) findViewById(R.id.pressureDropTextView);
 
         flowRateEditText = (EditText) findViewById(R.id.flowRateEditText);
-        widthEditText = (EditText) findViewById(R.id.widthEditText);
-        heightEditText = (EditText) findViewById(R.id.heightEditText);
+        diameterEditText = (EditText) findViewById(R.id.diameterEditText);
         temperatureEditText = (EditText) findViewById(R.id.temperatureEditText);
         roughnessEditText = (EditText) findViewById(R.id.roughnessEditText);
 
-        widthEditText.requestFocus();
-
+        diameterEditText.requestFocus();
     }
-
 
     public void printResult(View view) {
         // При нажатии на кнопку убирается клавиатура
@@ -71,17 +65,11 @@ public class AirDuctActivity extends AppCompatActivity {
         } catch (NumberFormatException e) {
             flowRateEditText.setText("Ошибка");
         }
-        Float width = null;
+        Float diameter = null;
         try {
-            width = Float.parseFloat(widthEditText.getText().toString());
+            diameter = Float.parseFloat(diameterEditText.getText().toString());
         } catch (NumberFormatException e) {
-            widthEditText.setText("Ошибка");
-        }
-        Float height = null;
-        try {
-            height = Float.parseFloat(heightEditText.getText().toString());
-        } catch (NumberFormatException e) {
-            heightEditText.setText("Ошибка");
+            diameterEditText.setText("Ошибка");
         }
         Float temperature = null;
         try {
@@ -96,28 +84,26 @@ public class AirDuctActivity extends AppCompatActivity {
             roughnessEditText.setText("Ошибка");
         }
 
-        if (flowRate.isNaN() || width.isNaN() || height.isNaN() || temperature.isNaN() || roughness.isNaN()){
-            diameterTextView.setText("Ошибка");
+        if (flowRate == null || diameter == null || temperature == null || roughness == null){
             velocityTextView.setText("Ошибка");
             pressureDropTextView.setText("Ошибка");
         } else {
-            AirDuctCalculator calc = new AirDuctCalculator(flowRate, width, height, temperature, roughness);
-            diameterTextView.setText(calc.getDiameter().toString());
+            AirDuctCalculator calc = new AirDuctCalculator(flowRate, diameter, temperature, roughness);
             velocityTextView.setText(calc.getVelocity().toString());
             pressureDropTextView.setText(calc.getPressureDrop().toString());
         }
+
 
     }
 
     public void clearFields(View view) {
         flowRateEditText.setText("");
-        widthEditText.setText("");
-        heightEditText.setText("");
+        diameterEditText.setText("");
+        diameterEditText.requestFocus();
         temperatureEditText.setText("");
         roughnessEditText.setText("");
-        widthEditText.requestFocus();
-        diameterTextView.setText("");
         velocityTextView.setText("");
         pressureDropTextView.setText("");
+
     }
 }
